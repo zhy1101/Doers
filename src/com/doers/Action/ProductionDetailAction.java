48bbfb00fdcaf1452f924d4cb7b32fe78ab4b332
zhy1101 <18073543511@163.com>
2017-12-08 21:02:10 +0800

    2017/12/8

---
+++ src/com/doers/Action/ProductionDetailAction.java
@@ -0,0 +1,32 @@
+package com.doers.Action;
+
+import com.doers.domain.Production;
+import com.doers.Service.ProductionService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.Map;
+
+public class ProductionDetailAction extends ActionSupport
+{
+  private String productionId;
+  private ProductionService productionService;
+
+  public void setProductionService(ProductionService productionService)
+  {
+    this.productionService = productionService;
+  }
+
+  public String getProductionId() {
+    return this.productionId;
+  }
+
+  public void setProductionId(String productionId) {
+    this.productionId = productionId;
+  }
+
+  public String showProductionDetail() {
+    Production p = this.productionService.getProductionById(this.productionId);
+    ActionContext.getContext().getSession().put("production", p);
+    return "gotoProductionDetail";
+  }
+}
