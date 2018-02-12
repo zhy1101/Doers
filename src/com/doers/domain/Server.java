aef9f5d7ffe7c4b49993bb14d98d032e1a0df697
zhy1101 <18073543511@163.com>
2018-02-12 16:09:23 +0800

    2018/2/12

---
+++ src/com/doers/domain/Server.java
@@ -0,0 +1,53 @@
+package com.doers.domain;
+
+public class Server {
+	private String serverId;
+	private String serverName;
+	private String serverDescribtion;
+	private Integer talkTime;
+	private String serverCover;
+	private Double lowLine;
+	private User user;
+	public String getServerId() {
+		return serverId;
+	}
+	public void setServerId(String serverId) {
+		this.serverId = serverId;
+	}
+	public String getServerName() {
+		return serverName;
+	}
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+	public String getServerDescribtion() {
+		return serverDescribtion;
+	}
+	public void setServerDescribtion(String serverDescribtion) {
+		this.serverDescribtion = serverDescribtion;
+	}
+	public Integer getTalkTime() {
+		return talkTime;
+	}
+	public void setTalkTime(Integer talkTime) {
+		this.talkTime = talkTime;
+	}
+	public String getServerCover() {
+		return serverCover;
+	}
+	public void setServerCover(String serverCover) {
+		this.serverCover = serverCover;
+	}
+	public Double getLowLine() {
+		return lowLine;
+	}
+	public void setLowLine(Double lowLine) {
+		this.lowLine = lowLine;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
